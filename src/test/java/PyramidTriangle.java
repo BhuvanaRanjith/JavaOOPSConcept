5f2c2021efaaaa93496ad624ff52bb7d3fda6ba0
Bhuvaneswari <mailtobhuvana04@gmail.com>
2023-06-27 18:01:15 -0700

    Printing the  1-5 loop number in the Pyramid Traingle format using inner forloop.

--- src/test/java/PyramidTriangle.java
+++ src/test/java/PyramidTriangle.java
@@ -1,17 +1,16 @@
 public class PyramidTriangle {
     public static void main(String[] args) {
-        int k=10;
-        for (int i = 0; i <=4 ; i++) {
-           System.out.print("\n");
-            for (int j = 1; j<=4-i; j++) {
-                System.out.print(k +" ");
-                k-- ;
+
+
+        int k = 10;
+        for (int i = 0; i <= 4; i++) {
+            System.out.print("\n");
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(k + " ");
+                k--;
             }
 
+
         }
-
-
     }
-
-}
-
+    }
