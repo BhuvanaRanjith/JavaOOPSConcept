2871087675e8badf3cdb4047970a839873d59b71
Bhuvaneswari <mailtobhuvana04@gmail.com>
2023-06-28 06:58:37 -0700

    Printing the number in 5 series . The number divide by 5.

--- src/test/java/PyramidTriangle.java
+++ src/test/java/PyramidTriangle.java
@@ -2,12 +2,13 @@ public class PyramidTriangle {
     public static void main(String[] args) {
 
 
-        int k = 10;
+        int k = 0;
         for (int i = 0; i <= 4; i++) {
             System.out.print("\n");
             for (int j = 1; j <= 4 - i; j++) {
+
                 System.out.print(k + " ");
-                k--;
+               k++;
             }
 
 
